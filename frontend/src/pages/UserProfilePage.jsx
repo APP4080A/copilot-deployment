65f80b7937a9e09e90b03b9437a015df654e8c70
Greta1998 <tangreta98@gmail.com>
2025-08-03 21:40:40 +0300

    profile page UI correct

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -313,8 +313,8 @@ export default function UserProfilePage() {
     }
 
     return (
-        <div className="bg-light min-vh-100 pb-5">
-            <main className="container pt-5">
+        <div className="d-flex flex-column min-vh-100 bg-light w-100">
+            <main className="container-fluid w-100 px-3 px-md-5 py-4">
                 {message && (
                     <div className={`alert alert-${message.type === 'success' ? 'success' : 'danger'} rounded-3`}
                          role="alert">
@@ -389,14 +389,7 @@ export default function UserProfilePage() {
                                     </svg>
                                     <span>Role: {user.role}</span>
                                 </div>
-                                <div className="d-flex align-items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="me-2" width="16" height="16"
-                                         fill="currentColor" viewBox="0 0 16 16">
-                                        <path
-                                            d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2z"/>
-                                    </svg>
-                                    <span>Joined: {user.joinDate}</span>
-                                </div>
+                            
                             </div>
                             <button
                                 className="btn fw-medium px-4 py-2"
